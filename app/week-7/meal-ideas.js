96e2d48ab50d6bf1a84081a908bd3b3bad74ef3e
Natalie <natalie.yeung16@gmail.com>
2024-07-01 23:22:49 -0600

    Define the fetching api function

--- app/week-7/meal-ideas.js
+++ app/week-7/meal-ideas.js
@@ -7,3 +7,17 @@ const MealIdeas = ({ ingredient }) => {
 };
 
 export default MealIdeas;
+
+const fetchMealIdeas = async (ingredient) => {
+  try {
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v1/1/filter.php?i=${ingredient}`
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch api.");
+    }
+    const data = await response.json();
+  } catch (error) {
+    console.error(error);
+  }
+};
